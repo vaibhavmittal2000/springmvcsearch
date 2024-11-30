376e70f8355a0a00ebdf7ce0bf5f34a867a84cb4
Vaibhav <vaibhavmittal2017@gmail.com>
2024-11-30 15:19:55 +0530

    search page using redirect and spring mvc

--- src/main/java/springmvcsearch/SearchController.java
+++ src/main/java/springmvcsearch/SearchController.java
@@ -1,5 +1,29 @@
 package springmvcsearch;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Controller
 public class SearchController {
 	
+	@RequestMapping("/home")
+	public String home() {
+		System.out.println("Going to home view..");
+		return "home";
+	}
+	
+	@RequestMapping("/search")
+	public RedirectView search(@RequestParam("querybox") String query) {
+		System.out.println("Inside search");
+		RedirectView redirectView = new RedirectView();
+		if (query == null || query.isBlank()) {
+            redirectView.setUrl("home");
+        } else {
+        	String url = "https://www.google.com/search?q="+query;
+            redirectView.setUrl(url);
+        }
+		return redirectView;
+	}
 }
